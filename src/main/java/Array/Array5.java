20743829cc9fda0838fc696777c5b2c75c3fd5b1
Pankaj Chavan <pankajchavan1501@gmail.com>
2026-01-07 11:42:14 +0530

    i have solved first  problem  with brute force and optimised  solution date:07/01/2026

--- src/main/java/Array/Array5.java
+++ src/main/java/Array/Array5.java
@@ -1,5 +1,8 @@
 package Array;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Array5 {
     /*You are given an array arr and an integer k. In one operation you can choose any element of array and decrease its value by k.
      *You need find the minimum number of operation such that all the elements in the array becomes less or equal to k.
@@ -13,23 +16,58 @@ public class Array5 {
      *
      *
      */
-    public static  int reducingWalls(int[] arr, int k) {
+    public static int reducingWalls(int[] arr, int k) {
         int count = 0;
-        for (int i = 0; i < arr.length ; i++) {
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] > k) {
-                count++;
+                int excess = arr[i] - k;
+                count += (excess + k - 1) / k;
             }
         }
         return count;
     }
 
+    /*  twosum
+     *Given an array arr[] of n integers and a target value, check if there exists a pair whose sum equals the target.
+     */
+    //brute force code tc is O(n) and sc is O(1) because you  have not used  extra dat structure.
+    public static int[] twosum(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    //optimised solution tc is O(n) and sc is O(n) because you  have  used  extra data structure. which is  HashMap
+    public static int[] twosum1(int[] arr, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            int num = target - arr[i];
+            if (map.containsKey(num)) {
+                return new int[]{map.get(num), i};
+            }
+            map.put(arr[i], i);
+        }
+        return new int[]{};
+    }
     public static void main(String[] agrs) {
-        int arr[] = {5, 3, 2, 6, 8};
+        int arr[] = {5, 3, 2, 6, 8 ,14};
         int k = 5;
         int m = reducingWalls(arr, k);
-        System.out.println(m);
-        System.out.println(m);
+        //System.out.println(m);
 
+        int arr1[] = {0, -1, 2, -3, 1};
+        int target = -2;
+        int[] kam = {2, 11, 15, 17, 7};
+        int t = 9;
+        int[] at = twosum1(arr1, target);
+        for (int num : at) {
+            System.out.println(num);
+        }
     }
 
 }
